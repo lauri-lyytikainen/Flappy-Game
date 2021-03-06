6da9b27399b46ab7fde7c74091a9babd07fd67fc
ExtraLongBaguette <lauri.lyytikainen1@gmail.com>
2021-03-06 12:06:13 +0200

    Added pipe logic and collisions

--- Levels/World.gd
+++ Levels/World.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
-
+onready var pipe_timer = get_node("pipe_timer")
 var pipe = preload("res://Objects/Pipe.tscn")
-
+var pipes = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	start_game()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	pass
+
+func start_game() -> void:
+	var p = pipe.instance()
+	pipes.append(p)
+	p.position = Vector2(250,250)
+	p.linear_velocity = Vector2(-100,0)
+	add_child(p)
+	pipe_timer.start()
