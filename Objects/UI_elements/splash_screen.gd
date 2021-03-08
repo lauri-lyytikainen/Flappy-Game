c9d2793a2c6465355fbdfb65d647d0315247c29b
ExtraLongBaguette <lauri.lyytikainen1@gmail.com>
2021-03-08 14:55:43 +0200

    Added custom splash screen

--- Objects/UI_elements/splash_screen.gd
+++ Objects/UI_elements/splash_screen.gd
@@ -1,4 +1,14 @@
 extends Node2D
 var next_scene = preload("res://Levels/World.tscn")
+onready var animation_player = get_node("AnimationPlayer")
+onready var logo = get_node("ColorRect/CenterContainer/VBoxContainer/TextureRect")
+
+func _ready() -> void:
+	pass
+
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
-	get_tree().change_scene_to(next_scene)
+	if "show_logo" in anim_name:
+		logo.texture.pause = false
+		animation_player.play("hide_logo")
+	elif "hide_logo" in anim_name:
+		get_tree().change_scene_to(next_scene)
